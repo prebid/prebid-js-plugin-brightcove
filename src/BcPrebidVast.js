0eaa980d2ee0e24f7f99ebebc53e027e257c7a2d
Yevgeniy Nisman <ynisman@appnexus.com>
2018-11-08 15:50:10 -0800

    VIDLA-4302 (Investigate solution to prevent contention between prebid for display and prebid for video in same document)

--- src/BcPrebidVast.js
+++ src/BcPrebidVast.js
@@ -18,20 +18,27 @@ var MOL_PLUGIN_URL = '//acdn.adnxs.com/video/plugins/mol/videojs_5.vast.vpaid.mi
 
 var $$PREBID_GLOBAL$$ = _prebidGlobal.getGlobal();
 
-_logger.always(_prefix, 'Version 0.2.6');
+_logger.always(_prefix, 'Version 0.2.6dev');
 
 var BC_prebid_in_progress = $$PREBID_GLOBAL$$.plugin_prebid_options && $$PREBID_GLOBAL$$.plugin_prebid_options.biddersSpec;
+
+var defaultTemplate = '<!DOCTYPE html>' +
+    '<html lang="en">' +
+    '<head><meta charset="UTF-8"></head>' +
+    '<body style="margin:0;padding:0">' +
+    '<script type="text/javascript">' +
+	'function notifyParent(succ) {' +
+    'window.parent.postMessage(succ ? \'ready\' : \'error\', \'{{origin}}\');' +
+	'}' +
+    '</script>' +
+    '<script type="text/javascript" src="{{iframePrebid_JS}}" onload="notifyParent(true);" onerror="notifyParent(false);"></script>' +
+    '</body>' +
+    '</html>';
 
 // the function does bidding and returns bids thru callback
 var BC_bidders_added = false;
 function doPrebid(options, callback) {
 	if ($$PREBID_GLOBAL$$.bc_pbjs && options.biddersSpec) {
-		// clean some prebid object properties
-		$$PREBID_GLOBAL$$.bc_pbjs.adUnits = [];
-		$$PREBID_GLOBAL$$.bc_pbjs.bidderSettings = {};
-		$$PREBID_GLOBAL$$.bc_pbjs.medianetGlobals = {};
-		BC_bidders_added = false;
-
 		$$PREBID_GLOBAL$$.bc_pbjs.que = $$PREBID_GLOBAL$$.bc_pbjs.que || [];
 
 		//
@@ -237,8 +244,27 @@ function loadPrebidScript(options, fromHeader) {
     	}
 	};
 
-	var temp_pbjs = window.pbjs || {};
-	if (temp_pbjs.requestBids) {
+	/* var getPbjs = function(obj, level) {
+		if (level <= 3) {
+			var intLevel = level;
+			for (var propName in obj) {
+				if (propName === 'pbjs') {
+					return obj.pbjs;
+				}
+				try {
+					if (typeof obj[propName] === 'object') {
+						getPbjs(obj[propName], intLevel + 1);
+					}
+				}
+				catch (e) {
+					console.log('failed on ' + propName);
+				}
+			}
+		}
+	};
+
+	var temp_pbjs = getPbjs(window, 0);
+	if (temp_pbjs && temp_pbjs.requestBids) {
 		// if prebid.js is already loaded try to invoke prebid.
 		$$PREBID_GLOBAL$$.bc_pbjs = pbjs;
 		doInternalPrebid();
@@ -271,7 +297,79 @@ function loadPrebidScript(options, fromHeader) {
     pbjsScr.type = 'text/javascript';
     pbjsScr.src = options.prebidPath ? options.prebidPath : DEFAULT_PREBID_JS_URL;
     var node = document.getElementsByTagName('head')[0];
-    node.appendChild(pbjsScr);
+    node.appendChild(pbjsScr); */
+
+	function getOrigin() {
+		if (window.location.origin) {
+			return window.location.origin;
+		}
+		else {
+			return window.location.protocol + '//' +
+				window.location.hostname +
+				(window.location.port ? ':' + window.location.port : '');
+		}
+	}
+
+	var frame = document.createElement('iframe');
+    frame.id = 'pbjs_conainer_' + Date.now();
+    frame.src = 'about:blank';
+    frame.marginWidth = '0';
+    frame.marginHeight = '0';
+    frame.frameBorder = '0';
+    frame.width = '0%';
+    frame.height = '0%';
+    frame.style.position = 'absolute';
+    frame.style.left = '0px';
+    frame.style.top = '0px';
+    frame.style.margin = '0px';
+    frame.style.padding = '0px';
+    frame.style.border = 'none';
+    frame.style.width = '0%';
+    frame.style.height = '0%';
+    document.body.appendChild(frame);
+
+	var template = defaultTemplate;
+    template = template.replace(new RegExp('{{iframePrebid_JS}}', 'g'), options.prebidPath ? options.prebidPath : DEFAULT_PREBID_JS_URL);
+	template = template.replace(new RegExp('{{origin}}', 'g'), getOrigin());
+
+    var iframeDoc = frame.contentWindow && frame.contentWindow.document;
+    if (iframeDoc) {
+		try {
+			iframeDoc.open();
+			iframeDoc.write(template);
+			iframeDoc.close();
+			var onLoad = function(msgEvent) {
+				// if (onLoad.apply(this, arguments)) {
+				//	clearTimeout(timeout);
+				// }
+				if (msgEvent.data === 'ready') {
+					$$PREBID_GLOBAL$$.bc_pbjs = frame.contentWindow.pbjs;
+					// after prebid.js is successfully loaded try to invoke prebid.
+					doInternalPrebid();
+				}
+				else {
+					// failed to load prebid.js.
+					_logger.error(_prefix, 'Failed to load prebid.js.');
+					if (options.pageNotificationCallback) {
+						options.pageNotificationCallback('message', 'Failed to load prebid.js');
+					}
+					$$PREBID_GLOBAL$$.bc_pbjs_error = true;
+					dispatchPrebidDoneEvent();
+				}
+			};
+
+			window.addEventListener('message', onLoad);
+		}
+		catch (e) {
+			// failed to load prebid.js.
+			_logger.error(_prefix, 'Failed to load prebid.js.');
+			if (options.pageNotificationCallback) {
+				options.pageNotificationCallback('message', 'Failed to load prebid.js. Error: ' + e);
+			}
+			$$PREBID_GLOBAL$$.bc_pbjs_error = true;
+			dispatchPrebidDoneEvent();
+		}
+	}
 }
 
 // this function loads MailOnline Plugin
