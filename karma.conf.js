3090db201d5e664b7e7258d8ed5c0bafb73d20e4
David Baldeschwieler <dbaldeschwieler@appnexus.com>
2019-03-22 17:34:48 -0700

    VSSPP-390 - Re-enabled coverage reports for unit tests.

--- karma.conf.js
+++ karma.conf.js
@@ -24,20 +24,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/**/*.js': ['commonjs'],
+        'src/**/*.js': ['commonjs', 'coverage'],
         'tests/e2e/auto/**/*.js': ['commonjs']
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-    // web server port
-    port: 9876,
+    reporters: ['progress', 'coverage'],
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+    // web server port
+    port: 9876,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
@@ -56,6 +56,15 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    coverageReporter: {
+      includeAllSources: true,
+      dir: 'coverage/',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    }
   })
 }
